d5dd0e83ac100fbcee850842d313d067ffa47fc7
DeathStalker34 <94406598+DeathStalker34@users.noreply.github.com>
2023-06-05 14:42:38 +0200

    Fix mobile footer

--- app/components/sidebar/MobileFooter.tsx
+++ app/components/sidebar/MobileFooter.tsx
@@ -32,7 +32,6 @@ const MobileFooter = () => {
                     href={route.href}
                     active={route.active}
                     icon={route.icon}
-                    onClick={route.onClick}
                 />
             ))}
         </div>
