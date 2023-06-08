42780b16e313dc76423f7040d71981d73990a7aa
DeathStalker34 <94406598+DeathStalker34@users.noreply.github.com>
2023-06-08 11:30:04 +0200

    Add contact button

--- app/components/listings/ListingInfo.tsx
+++ app/components/listings/ListingInfo.tsx
@@ -9,6 +9,11 @@ import dynamic from "next/dynamic";
 import { AiOutlineLike } from "react-icons/ai";
 import LikeButton from "../LikeButton";
 import getCurrentUser from "@/app/actions/getCurrentUser";
+import Button from "../Button";
+import { useCallback, useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import useLoginModal from "@/app/hooks/useLoginModal";
 
 const Map = dynamic(() => import('../Map'), {
     ssr: false
@@ -39,10 +44,31 @@ const ListingInfo: React.FC<ListingInfoProps> = ({
 }) => {
 
     const { getByValue } = useCountries()
-    // const usuario = getCurrentUser()
 
+    const router = useRouter()
     const nLikes = likes.length
     const coordinates = getByValue(locationValue)?.latlng
+
+    const [isLoading, setIsLoading] = useState(false)
+    const loginModal = useLoginModal()
+
+    const handleClick = useCallback(() => {
+
+        if (!currentUser) {
+            return loginModal.onOpen()
+        }
+
+        setIsLoading(true)
+
+        axios.post('/api/conversations', {
+            userId: user.id
+        })
+            .then((data) => {
+                router.push(`/conversations/${data.data.id}`)
+            })
+            .finally(() => setIsLoading(false))
+    }, [loginModal, currentUser, user, router])
+
 
     return (
         <div className="col-span-12 flex flex-col gap-8">
@@ -59,6 +85,7 @@ const ListingInfo: React.FC<ListingInfoProps> = ({
                     <div className="flex flex-row items-center gap-2">
                         <div>Servicio de {user?.name}</div>
                         <Avatar src={user?.image} />
+
                     </div>
 
                     <div className=" flex flex-row gap-2 items-center">
@@ -95,6 +122,13 @@ const ListingInfo: React.FC<ListingInfoProps> = ({
             </div>
             <hr />
             <Map center={coordinates} />
+            <hr />
+
+            <Button
+                label="Contactar con el usuario"
+                disabled={isLoading}
+                onClick={handleClick}
+            />
         </div>
     );
 }
